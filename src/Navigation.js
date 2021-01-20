c76da56f610795ac27f7ebdc8dc92b93ffb43a26
juiceghost <jg@renegade.se>
2021-01-20 08:14:07 +0100

    Start of day 6

---
+++ src/Navigation.js
@@ -0,0 +1,12 @@
+function Navigation() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    );
+}
+
+export default Navigation;
