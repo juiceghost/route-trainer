85da4fa44938af6ae771c68af3a4864600a52278
juiceghost <jg@renegade.se>
2021-01-20 09:47:58 +0100

    Added Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Navigation from './Navigation';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
 
 function App() {
     return (
         <Router>
             <div>Welcome to React-router!</div>
             <Navigation />
+            <Switch>
+                <Route path='/about'>
+                    <About />
+                </Route>
+                <Route path='/projects'>
+                    <Projects />
+                </Route>
+                <Route path='/contact'>
+                    <Contact />
+                </Route>
+
+            </Switch>
+
+
+            {/*  <About />
+            <Projects />
+            <Contact /> */}
+
         </Router>
     );
 }
