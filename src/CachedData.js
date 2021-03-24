8b68c397ffa5d6d92c57a0fa8274e7c63b5f6392
juiceghost <jg@renegade.se>
2021-03-24 08:38:02 +0100

    Modified cached data to be arrays

--- src/CachedData.js
+++ src/CachedData.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
-const weatherMalmo = {
+const timestamp = Date.now()
+const oneDay = 86400000;
+
+const weatherMalmo = [{
     "data": {
+        "timestamp": timestamp,
         "coord": {
             "lon": 13.0007,
             "lat": 55.6059
@@ -44,10 +48,146 @@ const weatherMalmo = {
         "name": "Malmo",
         "cod": 200
     }
-};
-
-const weatherUppsala = {
+}, {
     "data": {
+        "timestamp": timestamp + oneDay,
+        "coord": {
+            "lon": 13.0007,
+            "lat": 55.6059
+        },
+        "weather": [
+            {
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 5.51,
+            "feels_like": 0.86,
+            "temp_min": 2.78,
+            "temp_max": 6.11,
+            "pressure": 1018,
+            "humidity": 81
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 2.06,
+            "deg": 280
+        },
+        "clouds": {
+            "all": 20
+        },
+        "dt": 1616399518,
+        "sys": {
+            "type": 1,
+            "id": 1575,
+            "country": "SE",
+            "sunrise": 1616389492,
+            "sunset": 1616433886
+        },
+        "timezone": 3600,
+        "id": 2692969,
+        "name": "Malmo",
+        "cod": 200
+    }
+}, {
+    "data": {
+        "timestamp": timestamp + (oneDay * 2),
+        "coord": {
+            "lon": 13.0007,
+            "lat": 55.6059
+        },
+        "weather": [
+            {
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 6.88,
+            "feels_like": 0.86,
+            "temp_min": 2.78,
+            "temp_max": 6.11,
+            "pressure": 1018,
+            "humidity": 81
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 2.06,
+            "deg": 280
+        },
+        "clouds": {
+            "all": 20
+        },
+        "dt": 1616399518,
+        "sys": {
+            "type": 1,
+            "id": 1575,
+            "country": "SE",
+            "sunrise": 1616389492,
+            "sunset": 1616433886
+        },
+        "timezone": 3600,
+        "id": 2692969,
+        "name": "Malmo",
+        "cod": 200
+    }
+}, {
+    "data": {
+        "timestamp": timestamp + (oneDay * 3),
+        "coord": {
+            "lon": 13.0007,
+            "lat": 55.6059
+        },
+        "weather": [
+            {
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 3.11,
+            "feels_like": 0.86,
+            "temp_min": 2.78,
+            "temp_max": 6.11,
+            "pressure": 1018,
+            "humidity": 81
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 2.06,
+            "deg": 280
+        },
+        "clouds": {
+            "all": 20
+        },
+        "dt": 1616399518,
+        "sys": {
+            "type": 1,
+            "id": 1575,
+            "country": "SE",
+            "sunrise": 1616389492,
+            "sunset": 1616433886
+        },
+        "timezone": 3600,
+        "id": 2692969,
+        "name": "Malmo",
+        "cod": 200
+    }
+}];
+
+const weatherUppsala = [{
+    "data": {
+        "timestamp": timestamp,
         "coord": {
             "lon": 17.75,
             "lat": 60
@@ -90,10 +230,146 @@ const weatherUppsala = {
         "name": "Uppsala County",
         "cod": 200
     }
-}
-
-const weatherGoteborg = {
+}, {
     "data": {
+        "timestamp": timestamp + oneDay,
+        "coord": {
+            "lon": 17.75,
+            "lat": 60
+        },
+        "weather": [
+            {
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 3.83,
+            "feels_like": -3.04,
+            "temp_min": 2,
+            "temp_max": 3.33,
+            "pressure": 1009,
+            "humidity": 70
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 5.14,
+            "deg": 290
+        },
+        "clouds": {
+            "all": 20
+        },
+        "dt": 1616398622,
+        "sys": {
+            "type": 1,
+            "id": 1731,
+            "country": "SE",
+            "sunrise": 1616388263,
+            "sunset": 1616432836
+        },
+        "timezone": 3600,
+        "id": 2666218,
+        "name": "Uppsala County",
+        "cod": 200
+    }
+}, {
+    "data": {
+        "timestamp": timestamp + (oneDay * 2),
+        "coord": {
+            "lon": 17.75,
+            "lat": 60
+        },
+        "weather": [
+            {
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 7.83,
+            "feels_like": -3.04,
+            "temp_min": 2,
+            "temp_max": 3.33,
+            "pressure": 1009,
+            "humidity": 70
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 5.14,
+            "deg": 290
+        },
+        "clouds": {
+            "all": 20
+        },
+        "dt": 1616398622,
+        "sys": {
+            "type": 1,
+            "id": 1731,
+            "country": "SE",
+            "sunrise": 1616388263,
+            "sunset": 1616432836
+        },
+        "timezone": 3600,
+        "id": 2666218,
+        "name": "Uppsala County",
+        "cod": 200
+    }
+}, {
+    "data": {
+        "timestamp": timestamp + (oneDay * 3),
+        "coord": {
+            "lon": 17.75,
+            "lat": 60
+        },
+        "weather": [
+            {
+                "id": 801,
+                "main": "Clouds",
+                "description": "few clouds",
+                "icon": "02d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 9.83,
+            "feels_like": -3.04,
+            "temp_min": 2,
+            "temp_max": 3.33,
+            "pressure": 1009,
+            "humidity": 70
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 5.14,
+            "deg": 290
+        },
+        "clouds": {
+            "all": 20
+        },
+        "dt": 1616398622,
+        "sys": {
+            "type": 1,
+            "id": 1731,
+            "country": "SE",
+            "sunrise": 1616388263,
+            "sunset": 1616432836
+        },
+        "timezone": 3600,
+        "id": 2666218,
+        "name": "Uppsala County",
+        "cod": 200
+    }
+}];
+
+const weatherGoteborg = [{
+    "data": {
+        "timestamp": timestamp,
         "coord": {
             "lon": 11.9668,
             "lat": 57.7072
@@ -136,7 +412,142 @@ const weatherGoteborg = {
         "name": "Gothenburg",
         "cod": 200
     }
-}
+}, {
+    "data": {
+        "timestamp": timestamp + oneDay,
+        "coord": {
+            "lon": 11.9668,
+            "lat": 57.7072
+        },
+        "weather": [
+            {
+                "id": 800,
+                "main": "Clear",
+                "description": "clear sky",
+                "icon": "01d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 6.5,
+            "feels_like": 0.2,
+            "temp_min": 2,
+            "temp_max": 6.67,
+            "pressure": 1015,
+            "humidity": 80
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 3.6,
+            "deg": 220
+        },
+        "clouds": {
+            "all": 0
+        },
+        "dt": 1616400053,
+        "sys": {
+            "type": 1,
+            "id": 1746,
+            "country": "SE",
+            "sunrise": 1616389699,
+            "sunset": 1616434175
+        },
+        "timezone": 3600,
+        "id": 2711537,
+        "name": "Gothenburg",
+        "cod": 200
+    }
+}, {
+    "data": {
+        "timestamp": timestamp + (oneDay * 2),
+        "coord": {
+            "lon": 11.9668,
+            "lat": 57.7072
+        },
+        "weather": [
+            {
+                "id": 800,
+                "main": "Clear",
+                "description": "clear sky",
+                "icon": "01d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 3.5,
+            "feels_like": 0.2,
+            "temp_min": 2,
+            "temp_max": 6.67,
+            "pressure": 1015,
+            "humidity": 80
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 3.6,
+            "deg": 220
+        },
+        "clouds": {
+            "all": 0
+        },
+        "dt": 1616400053,
+        "sys": {
+            "type": 1,
+            "id": 1746,
+            "country": "SE",
+            "sunrise": 1616389699,
+            "sunset": 1616434175
+        },
+        "timezone": 3600,
+        "id": 2711537,
+        "name": "Gothenburg",
+        "cod": 200
+    }
+}, {
+    "data": {
+        "timestamp": timestamp + (oneDay * 3),
+        "coord": {
+            "lon": 11.9668,
+            "lat": 57.7072
+        },
+        "weather": [
+            {
+                "id": 800,
+                "main": "Clear",
+                "description": "clear sky",
+                "icon": "01d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": -2.5,
+            "feels_like": 0.2,
+            "temp_min": 2,
+            "temp_max": 6.67,
+            "pressure": 1015,
+            "humidity": 80
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 3.6,
+            "deg": 220
+        },
+        "clouds": {
+            "all": 0
+        },
+        "dt": 1616400053,
+        "sys": {
+            "type": 1,
+            "id": 1746,
+            "country": "SE",
+            "sunrise": 1616389699,
+            "sunset": 1616434175
+        },
+        "timezone": 3600,
+        "id": 2711537,
+        "name": "Gothenburg",
+        "cod": 200
+    }
+}];
 
 //console.log(weatherUppsala);
 //console.log(weatherMalmo);
@@ -160,7 +571,7 @@ const weatherGoteborg = {
     }
 }]
 */
-
+/*
 const weatherArr = [weatherUppsala, weatherMalmo, weatherGoteborg]
 
 let resultArr = [];
@@ -190,7 +601,7 @@ const CachedData = () => (
 );
 */
 const CachedData = () => {
-    const [result, setResult] = useState(newResult)
+    const [result, setResult] = useState([])
 
     const handleChange = (name) => {
         console.log(name);
@@ -199,7 +610,7 @@ const CachedData = () => {
         setResult(filteredResult)
     }
 
-    const resetHidden = () => setResult(newResult);
+    const resetHidden = () => setResult([]);
 
     // Vid klick på HIDE skall handleChange i CachedData's scope köras.
     // överkurs: jag vill att motsvarande name som hör till raden där knappen sitter skrivs ut
